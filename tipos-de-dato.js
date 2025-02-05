71dfe8cfe493c927c314ceb1c05428b2497cc1b4
ElenaGS1702 <egarcias1702@gmail.com>
2025-02-04 20:34:34 -0600

    se agregan ejemplos de tipos de datos

---
+++ tipos-de-dato.js
@@ -0,0 +1,33 @@
+console.log("Hola mundo");
+
+var nombre = "Ana";
+var edad = 23;
+var sueldo = 1.234;
+var casada = false;
+var vacunas = true;
+var hijos = null;
+var dato;
+
+console.log(typeof(nombre));
+console.log(typeof(edad));
+console.log(typeof(sueldo));
+console.log(typeof(casada));
+console.log(typeof(vacunas));
+console.log(typeof(hijos));
+console.log(typeof(dato));
+
+console.log(typeof(42));
+console.log(typeof('Veinticinco'));
+console.log(typeof(-666));
+console.log(typeof(true));
+console.log(typeof(0));
+console.log(typeof(''));
+console.log(typeof(null));
+console.log(typeof(undefined));
+console.log(typeof(FALSE));
+
+console.log(typeof("1234"));
+console.log(typeof("false"));
+console.log(typeof(0.02648));
+console.log(typeof("true"));
+console.log(typeof(hola));
